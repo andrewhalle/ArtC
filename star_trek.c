e056ab214ccad347f75eaf24916a98e11f4f891c
Andrew Halle <ahalle@berkeley.edu>
2016-11-17 17:32:05 -0800

    switched star trek to organ - simple

--- star_trek.c
+++ star_trek.c
@@ -3,15 +3,13 @@
 
 int main() {
 	LinkedList* waveform = newWaveform();
-	addSineWave(waveform, 349.23, 0.6);
-	printf("1");
-	addSineWave(waveform, 622.25, 0.9);
-	printf("1");
-	addSineWave(waveform, 587.33, 0.3);
-	addSineWave(waveform, 523.25, 0.201);
-	addSineWave(waveform, 466.16, 0.201);
-	addSineWave(waveform, 440, 0.201);
-	addSineWave(waveform, 415.30, 1.2);
+	addOrgan(waveform, 349.23, 0.6);
+	addOrgan(waveform, 622.25, 0.9);
+	addOrgan(waveform, 587.33, 0.3);
+	addOrgan(waveform, 523.25, 0.201);
+	addOrgan(waveform, 466.16, 0.201);
+	addOrgan(waveform, 440, 0.201);
+	addOrgan(waveform, 415.30, 1.2);
 	writeSoundFile("star_trek.wav", waveform);
 	freeWaveform(waveform);
 }
